8fbb5479985d71833c6dd5db977b066231fd1999
Anshu Priya <anshupriya31@gmail.com>
2023-09-18 01:51:03 +0530

    Addedx logic for Filter using React Hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("BUtton clicked");
+            const filteredList = listOfRestaurants.filter(
+              (resList) => resList.info.avgRating > 4
+            );
+            setListOfRestaurants(filteredList);
           }}
         >
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
