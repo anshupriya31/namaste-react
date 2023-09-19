a334f5954bf129e308ae8a3485427b5117215e19
Anshu Priya <anshupriya31@gmail.com>
2023-09-19 23:07:38 +0530

    Adding Shimmer UI/skeleton of the UI as loader

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,29 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  
   const fetchData = async () => {
     const res = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      );
-      const data = await res.json();
-      setListOfRestaurants(data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants.map((res) => res));
-    };
-    
-    useEffect(() => {
-      fetchData();
-    }, []);
+    );
+    const data = await res.json();
+    setListOfRestaurants(
+      data?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.map(
+        (res) => res
+      )
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
