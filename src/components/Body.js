84ddcf0a0b7ba11619411285e1ecc2c8013a8d61
Anshu Priya <anshupriya31@gmail.com>
2023-09-20 23:32:54 +0530

    Adding conditinal rendering to Body Component

--- src/components/Body.js
+++ src/components/Body.js
@@ -21,11 +21,7 @@ const Body = () => {
     fetchData();
   }, []);
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
-
-  return (
+  return listOfRestaurants.length === 0 ? <Shimmer /> : (
     <div className="body">
       <div className="filter">
         <button
