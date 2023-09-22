ba3a001011ad3a2174ff25c2b6d9859d77db7320
Anshu Priya <anshupriya31@gmail.com>
2023-09-22 23:58:40 +0530

    Feature | Route | Added routing to the application by using React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const AppLayout = () => {
-    return(
-        <div className="app">
-            <Header /> 
-            <Body />  
-        </div>
-        )
-    };
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />)
-
-// https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.341351&lng=76.66708349999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
+root.render(<RouterProvider router={appRouter} />);
