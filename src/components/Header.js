01b7c26c2836367a9633cff34b04828e4c18a95c
Anshu Priya <anshupriya31@gmail.com>
2023-09-30 23:56:34 +0530

    Modify | Tailwind | Adding CSS to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,34 +8,34 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   return (
-    <div className="header">
+    <div className="flex justify-between shadow-lg m-2">
       <div className="logo-container">
-        <img className="logo" src={LOGO_URL} alt="Image not loading" />
+        <img className="w-56" src={LOGO_URL} alt="Image not loading" />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
-          <li>
+      <div className="flex items-center">
+        <ul className="flex p-10 m-10">
+          <li className="px-4">Online Status: {onlineStatus ? "🟢" : "🔴"}</li>
+          <li className="px-4">
             <Link className="remove-link" to="/">
               Home
             </Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link className="remove-link" to="/about">
               About Us
             </Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link className="remove-link" to="/contact">
               Contact Us
             </Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link className="remove-link" to="/grocery">
               Grocery
             </Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
